89ac36b405fe69bd607d358351d75d76c46ea74c
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-03-06 19:10:10 +0100

    configure canvas for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, {isServer}) => {
+        // this adds a rule to handle the canvas.node binary module
+        config.module.rules.push({
+            test: /\.node$/, use: 'raw-loader'
+        })
+
+        // exclude canvas form being processed by nextjs in the browser
+        if(!isServer) config.externals.push('çanvas')
+        return config
+    }
+};
 
 export default nextConfig;
