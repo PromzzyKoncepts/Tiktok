b4c5cfae119312cf6ac512c312deb0041a4c5faa
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-03-06 19:13:12 +0100

    wrap the home with main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import MainLayout from "./layouts/MainLayout";
 
 export default function Home() {
   return (
-    <main>
-      Home
-    </main>
+    <>
+      <MainLayout>
+        <div>HOME</div>
+      </MainLayout>
+    </>
   );
 }
