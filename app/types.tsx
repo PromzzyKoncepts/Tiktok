0c858568eae20121649502aedbc62ed1d69db933
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-03-10 20:01:57 +0100

    add interfaces for postmain components

--- app/types.tsx
+++ app/types.tsx
@@ -20,20 +20,45 @@ export interface RandomUsers {
 export interface PostWithProfile {
   id: string;
   user_id: string;
-  video_url: string,
-  text: string,
-  created_at: string,
+  video_url: string;
+  text: string;
+  created_at: string;
   profile: {
-    user_id: string,
-    name: string,
-    username: string,
-    image: string,
+    user_id: string;
+    name: string;
+    username: string;
+    image: string;
   };
 }
+
+export interface formatNumber {
+  num: number,
+  precision: number
+}
+
 
 export interface postMainCompTypes {
   post: PostWithProfile;
 }
+
+export interface postMainLikesCompTypes {
+  post: PostWithProfile;
+}
+
+export interface Like {
+  id:string,
+  user_id: string,
+  post_id: string,
+}
+
+export interface Comment {
+  id:string,
+  user_id: string,
+  post_id: string,
+  text: string,
+  created_at: string
+}
+
 
 export interface MenuItemFollowCompTypes {
   user: RandomUsers;
