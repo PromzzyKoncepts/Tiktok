4aac35607ff0f71690649df4ec9d4549e32d1fdb
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-04-20 16:12:59 +0100

    fix build error

--- app/types.tsx
+++ app/types.tsx
@@ -88,8 +88,17 @@ export interface Post {
   created_at: string;
 }
 
-export CommentWithProfile {
-  
+export interface CommentWithProfile {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+  profile: {
+      user_id: string;
+      name: string;
+      image: string;
+  }
 }
 export interface Like {
   id: string;
