a50d8cc46e865babed29d3d57e604bd7287eecd2
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-04-23 15:51:10 +0100

    add name for refererncing

--- app/components/Profile/TextInput.tsx
+++ app/components/Profile/TextInput.tsx
@@ -1,13 +1,14 @@
 import { TextInputCompTypes } from '@/app/types'
 import React from 'react'
 
-const TextInput = ({string, inputType, placeholder, error, onUpdate}: TextInputCompTypes) => {
+const TextInput = ({string, name, inputType, placeholder, error, onUpdate}: TextInputCompTypes) => {
 
   
   return (
     <div>
       <input type={inputType} 
       autoComplete="off"
+      name={name}
       className="block w-full border-gray-300 focus:outline-none bg-[#f1f1f2] text-gray-800  border py-2.5 px-3 rounded-md " 
       placeholder={placeholder} 
       value={string || ""} 
