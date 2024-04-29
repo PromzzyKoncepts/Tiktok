83f38e45c3ce8df6c46b862ac8a3bfa549297082
Promise Okechukwu <okechukwupromise638@gmail.com>
2024-04-29 11:58:29 +0100

    create hook to update user profile image

---
+++ app/hooks/useUpdateProfileImage.tsx
@@ -0,0 +1,19 @@
+import { database, Query } from "@/libs/AppWriteClient";
+import React from "react";
+
+const useUpdateProfileImage = async (id: string, image: string) => {
+  try {
+    await database.updateDocument(
+      String(process.env.NEXT_PUBLIC_DATABASE_ID),
+      String(process.env.NEXT_PUBLIC_COLLECTION_ID_PROFILE),
+      id,
+      {
+        image: image,
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default useUpdateProfileImage;
